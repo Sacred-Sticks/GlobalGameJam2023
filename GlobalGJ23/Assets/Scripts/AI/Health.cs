146fe40cbf001151cc6a7f93ffcfa0fe069258d9
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2023-02-05 11:32:05 -0600

    Updated Combat

--- GlobalGJ23/Assets/Scripts/AI/Health.cs
+++ GlobalGJ23/Assets/Scripts/AI/Health.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] public int health;
+    [SerializeField] private UnityEvent onDeath;
 
     public void ChangeHealth(int healthModification)
     {
         health += healthModification;
         if (health < 1) {
-            Destroy(gameObject);
+            onDeath.Invoke();
         }
     }
 }
