5e952ae2036bb8e16450d5bb260a7b32e8ab05e2
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2023-02-05 15:38:36 -0600

    Finished Combat

--- GlobalGJ23/Assets/Scripts/AI/Health.cs
+++ GlobalGJ23/Assets/Scripts/AI/Health.cs
@@ -1,11 +1,35 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using static UnityEngine.GraphicsBuffer;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] public int health;
     [SerializeField] private UnityEvent onDeath;
     [SerializeField] private StatusObject status;
+    [SerializeField] private int damagePerHit;
+    [SerializeField] private float attackDelay;
+
+    private bool canAttack = true;
+
+    [SerializeField] private LayerMask attackableLayers;
+
+    private void OnTriggerEnter(Collider collision) {
+        if (attackableLayers == (attackableLayers | (1 << collision.gameObject.layer))) {
+            if (!canAttack) {
+                return;
+            }
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack() {
+        ChangeHealth(-damagePerHit);
+        canAttack = false;
+        yield return new WaitForSeconds(attackDelay);
+        canAttack = true;
+    }
 
     public void ChangeHealth(int healthModification)
     {
@@ -13,13 +37,5 @@ public class Health : MonoBehaviour
         if (health < 1) {
             onDeath.Invoke();
         }
-    }
-
-    public void playerDeath() {
-        status.level = StatusObject.Level.AdultYears;
-    }
-
-    public void enemyDeath() {
-        status.level = StatusObject.Level.Menu2;
     }
 }
